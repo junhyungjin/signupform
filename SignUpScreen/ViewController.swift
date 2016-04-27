6ca16ff512207a9094875ac47b053a876cc129bb
Jun Hyung Jin <junhyungjin@gmail.com>
2016-04-27 13:22:14 +0900

    signup form

--- SignUpScreen/ViewController.swift
+++ SignUpScreen/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var idTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var telTextField: UITextField!
+    @IBOutlet var blogTextField: UITextField!
+    @IBOutlet var resultTextView: UITextView!
+    
+    @IBAction func signupButton(sender: AnyObject) {
+        resultTextView.text = "\(nameTextField.text!)님 가입을 축하합니다."
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print("textFieldShouldReturn")
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
